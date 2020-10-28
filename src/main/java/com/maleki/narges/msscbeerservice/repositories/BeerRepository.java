4c1eecdf20e6ee4bb24ceb0fa740bfc2b7398f1a
narges_sh_maleki <narges.sh.maleki@gmail.com>
2020-10-28 10:28:57 +0100

    Closes #6

---
+++ src/main/java/com/maleki/narges/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.maleki.narges.msscbeerservice.repositories;
+
+import com.maleki.narges.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}

55718f8ada299e335b4655b611a0f498f48e6190
narges_sh_maleki <narges.sh.maleki@gmail.com>
2020-11-14 14:58:25 +0100

    connected to mysql and added localmysql  profile

--- src/main/java/com/maleki/narges/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/maleki/narges/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,22 +2,20 @@ package com.maleki.narges.msscbeerservice.bootstrap;
 
 import com.maleki.narges.msscbeerservice.domain.Beer;
 import com.maleki.narges.msscbeerservice.repositories.BeerRepository;
+import com.maleki.narges.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-//It is commented!!! so the data is loaded via data.sql because we cannot generate Id by ourself since hibernate autogenerat
-//@Component
+//It was commented. so the data is loaded via data.sql because we cannot generate Id by ourself since hibernate autogenerat
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public final static String BEER_1_UPC = "0631234200036";
     public final static String BEER_2_UPC = "0631234300019";
     public final static String BEER_3_UPC = "0083783375213";
-    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final BeerRepository beerRepository;
 
@@ -31,7 +29,7 @@ public class BeerLoader implements CommandLineRunner {
 
             beerRepository.save(Beer.builder()
                     .beerName("Mango Bobs")
-                    .beerStyle("HEINEKEN")
+                    .beerStyle(BeerStyleEnum.IPA.name())
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_1_UPC)
@@ -41,7 +39,7 @@ public class BeerLoader implements CommandLineRunner {
 
             beerRepository.save(Beer.builder()
                     .beerName("Galaxy cat")
-                    .beerStyle("CORONA")
+                    .beerStyle(BeerStyleEnum.HEINEKEN.name())
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_2_UPC)
@@ -51,7 +49,7 @@ public class BeerLoader implements CommandLineRunner {
 
             beerRepository.save(Beer.builder()
                     .beerName("Galaxy cat")
-                    .beerStyle("CORONA")
+                    .beerStyle(BeerStyleEnum.PALE_ALE.name())
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_3_UPC)
